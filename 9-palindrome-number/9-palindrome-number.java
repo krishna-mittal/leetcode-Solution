c7270fac0070556f0c0c2a252d1ce3bcbe37e397
Krishna Mittal <74191940+Krishnamittal9119@users.noreply.github.com>
2022-09-19 20:17:51 +0530

    Time: 15 ms (55.83%), Space: 44 MB (80.05%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0)
-        {
+        if(x < 0 || (x % 10 == 0 && x != 0))
             return false;
-        }
-        int a = 0;
-        int y = 0;
-        int z = x;
         
-        while(x != 0)
+        int digit = 0;
+        while(x > digit)
         {
-            y = x%10;
-            a = a * 10 + y;
-            x = x / 10 ;
+            // System.out.println(digit);
+            digit = digit * 10 + x % 10;
+            // System.out.println(digit);
+            x /= 10;
         }
-        if(a == z)
-        {
-            return true;
-        }
-        return false;
+        // System.out.println(x);
+        
+        return (x == digit || x == (digit / 10));
     }
 }
