d8be4015b243ed8bd230d1649291d74b7adab585
Krishna Mittal <74191940+Krishnamittal9119@users.noreply.github.com>
2022-09-21 10:27:59 +0530

    Time: 10 ms (46.38%), Space: 44.9 MB (50.53%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,30 +1,32 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
         
-        int[][] dp = new int[n + 1][m + 1];
+        int m = word1.length();
+        int n = word2.length();
         
-        for(int i = 0; i < n + 1; i++)
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for(int i = 0; i < m + 1; i++)
         {
             dp[i][0] = i;
         }
         
-        for(int i = 0; i < m + 1; i++)
+        for(int j = 0; j < n + 1; j++)
         {
-            dp[0][i] = i;
+            dp[0][j] = j;
         }
-        for(int i = 1; i < n + 1; i++)
+        
+        for(int i = 1; i < m + 1; i++)
         {
-            for(int j = 1; j < m + 1; j++)
+            for(int j = 1; j < n + 1; j++)
             {
                 if(word1.charAt(i - 1) == word2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[i][j] = dp[i -1][j - 1];
                 else
                     dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j],dp[i][j - 1]), dp[i - 1][j - 1]);
             }
         }
-        return dp[n][m];
+        return dp[m][n];
         
         
         // return distance(word1, word2, word1.length(), word2.length());
