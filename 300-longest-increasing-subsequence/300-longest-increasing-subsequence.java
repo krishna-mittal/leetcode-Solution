3fd169ac057f16caa2c5471e5e76b502b806d345
Krishna Mittal <74191940+Krishnamittal9119@users.noreply.github.com>
2022-09-21 14:49:42 +0530

    Time: 5 ms (87.53%), Space: 43.9 MB (82.06%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,20 +1,75 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
+//         int[] dp = new int[nums.length];
+//         dp[0] = 1;
         
-        int ans  = 1;
+//         int ans  = 1;
+//         for(int i = 1; i < nums.length; i++)
+//         {
+//             int max = 0;
+//             for(int j = 0; j < i; j++)
+//             {
+//                 if(nums[i] > nums[j])
+//                     max = Math.max(max, dp[j]);
+//                 dp[i] = max + 1;
+//                 ans = Math.max(ans, dp[i]);
+//             }
+//         }
+//         return ans;
+        
+        int[] arr = new int[nums.length];
+        arr[0] = nums[0];
+        int len = 1;
+        
         for(int i = 1; i < nums.length; i++)
         {
-            int max = 0;
-            for(int j = 0; j < i; j++)
+            if(arr[len - 1] < nums[i])
             {
-                if(nums[i] > nums[j])
-                    max = Math.max(max, dp[j]);
-                dp[i] = max + 1;
-                ans = Math.max(ans, dp[i]);
+                arr[len] = nums[i];
+                len++;
+            }
+            else
+            {
+                int c = Indceil(arr, 0 , len - 1, nums[i]);
+                arr[c] = nums[i];
             }
         }
-        return ans;
+        return len;
      }
+    public int Indceil(int arr[], int r, int l, int m)
+    {
+        while(l > r)
+        {
+            int x = r + (l - r)/2;
+            if(arr[x] >= m)
+                l = x;
+            else
+                r = x + 1;
+        }
+        return l;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
