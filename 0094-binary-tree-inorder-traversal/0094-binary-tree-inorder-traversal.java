33000f6ae9efcd8c42c1d72fdf96ca5250a75551
Krishna Mittal <74191940+Krishnamittal9119@users.noreply.github.com>
2022-10-13 10:52:29 +0530

    Time: 1 ms (47.15%), Space: 41.8 MB (79.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -24,16 +24,57 @@ class Solution {
         // System.out.println(root.val+" ");
         // inorderTraversal(root.right);
         
-        List<Integer> res  = new ArrayList<>();
-        helper(root, res);
-        return res;
-    }
-    private void helper(TreeNode node, List<Integer> list)
-    {
-        if(node == null){return ;}
+//         List<Integer> res  = new ArrayList<>();
+//         helper(root, res);
+//         return res;
+//     }
+//     private void helper(TreeNode node, List<Integer> list)
+//     {
+//         if(node == null){return ;}
         
-        helper(node.left, list);
-        list.add(node.val);
-        helper(node.right, list);
+//         helper(node.left, list);
+//         list.add(node.val);
+//         helper(node.right, list);
+        
+        
+        
+//         if(root == null)
+// 	        return;
+
+//         Stack<TreeNode> stack = new Stack<>();
+
+//         TreeNode temp =  root;
+
+//         while(!stack.isEmpty() || temp != null)
+//         {
+// 	        if(temp != null)
+// 	        {
+// 		        stack.push(temp);
+// 		        temp  = temp.left;
+// 	        }	
+// 	        else
+// 	        {
+// 		        temp = stack.pop();
+// 		        System.out.println(temp.data + " ");
+// 		        temp = temp.right;
+// 	        }
+//         }
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while(curr != null || !stack.isEmpty())
+        {
+            while(curr != null)
+            {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+        
+        
     }
 }
